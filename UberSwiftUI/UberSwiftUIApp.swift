89d159d3b1e1d84b0294b4379d5c74b4899b44e8
Oleg Panchenko <77533590+Oleg-Panchenko@users.noreply.github.com>
2023-08-22 22:02:08 +0300

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Panchenko Oleg on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
